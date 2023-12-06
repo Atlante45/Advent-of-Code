c59f452ded9c7d52bcacc3123474862258c8923e
Atlante45 <clement.brisset@gmail.com>
2023-12-05 22:03:00 -0800

    2023 Day 6 refactor

--- solutions/y2023/d06.py
+++ solutions/y2023/d06.py
@@ -1,4 +1,20 @@
-from itertools import accumulate
+from math import ceil, floor, prod, sqrt
+
+
+def solve_eq(a, b, c):
+    d = sqrt(b * b - 4 * a * c)
+    r0 = (-b + d) / (2 * a)
+    r1 = (-b - d) / (2 * a)
+    return (r0, r1)
+
+
+def win_ways(time, dist):
+    # t * (time - t) > dist
+    # -t*t + time * t - dist > 0
+    r0, r1 = solve_eq(-1, time, -dist)
+    r0 = floor(r0 + 1)
+    r1 = ceil(r1 - 1)
+    return r1 - r0 + 1
 
 
 def parse(data):
@@ -7,28 +23,13 @@ def parse(data):
 
 def part1(lines):
     lines = list(zip(*[map(int, line.split()[1:]) for line in lines]))
-
-    res = 1
-    for time, dist in lines:
-        ways = 0
-        for t in range(time):
-            if t * (time - t) > dist:
-                ways += 1
-
-        res *= ways
-
-    return res
+    return prod(win_ways(time, dist) for time, dist in lines)
 
 
 def part2(lines):
     time = int(lines[0].split(":")[1].replace(" ", ""))
     dist = int(lines[1].split(":")[1].replace(" ", ""))
-
-    ways = 0
-    for t in range(time):
-        if t * (time - t) > dist:
-            ways += 1
-    return ways
+    return win_ways(time, dist)
 
 
 TEST_DATA = {}
