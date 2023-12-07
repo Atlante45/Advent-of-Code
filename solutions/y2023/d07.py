64a5f23740c21e1ec675736e64e7934afb5f681e
Atlante45 <clement.brisset@gmail.com>
2023-12-06 21:50:18 -0800

    2023 Day 7 refactor

--- solutions/y2023/d07.py
+++ solutions/y2023/d07.py
@@ -1,88 +1,55 @@
 from collections import defaultdict
-from itertools import count
-
-CARDS = "23456789TJQKA"
-CARDS_ALT = "J23456789TQKA"
 
 
 def order(hand):
+    CARDS = "23456789TJQKA"
     cards = defaultdict(int)
 
-    score = 0
+    cards_strength = 0
     for c in hand:
-        score = 100 * score + CARDS.index(c)
+        cards_strength = 100 * cards_strength + CARDS.index(c)
         cards[c] += 1
 
-    counts = sorted(cards.values())
+    counts = sorted(cards.values(), reverse=True) + [0]
 
-    if counts[-1] == 5:
-        score += 70000000000
-    elif counts[-1] == 4:
-        score += 60000000000
-    elif counts[-1] == 3 and counts[-2] == 2:
-        score += 50000000000
-    elif counts[-1] == 3:
-        score += 40000000000
-    elif counts[-1] == 2 and counts[-2] == 2:
-        score += 30000000000
-    elif counts[-1] == 2:
-        score += 20000000000
-    else:
-        score += 10000000000
+    hand_strength = 10 * counts[0] + counts[1]
 
-    return score
+    return hand_strength * pow(100, len(hand)) + cards_strength
 
 
 def order_alt(hand):
+    CARDS = "J23456789TQKA"
     cards = defaultdict(int)
 
-    score = 0
+    cards_strength = 0
     for c in hand:
-        score = 100 * score + CARDS_ALT.index(c)
+        cards_strength = 100 * cards_strength + CARDS.index(c)
         cards[c] += 1
 
     j_count = cards["J"]
     del cards["J"]
-    counts = sorted(cards.values())
+    counts = sorted(cards.values(), reverse=True) + [0, 0]
 
-    if j_count == 5 or (counts[-1] + j_count) == 5:
-        score += 70000000000
-    elif (counts[-1] + j_count) == 4:
-        score += 60000000000
-    elif (counts[-1] + j_count) == 3 and counts[-2] == 2:
-        score += 50000000000
-    elif counts[-1] == 3 and (counts[-2] + j_count) == 2:
-        score += 50000000000
-    elif (counts[-1] + j_count) == 3:
-        score += 40000000000
-    elif counts[-1] == 2 and (counts[-2] + j_count) == 2:
-        score += 30000000000
-    elif (counts[-1] + j_count) == 2:
-        score += 20000000000
-    else:
-        score += 10000000000
+    hand_strength = 10 * (counts[0] + j_count) + counts[1]
 
-    return score
+    return hand_strength * pow(100, len(hand)) + cards_strength
 
 
 def parse(data):
-    return data.splitlines()
+    return [line.split() for line in data.splitlines()]
+
+
+def solve(lines, ordering):
+    hands = sorted(lines, key=lambda v: ordering(v[0]))
+    return sum((i + 1) * int(hand[1]) for i, hand in enumerate(hands))
 
 
 def part1(lines):
-    lines = [line.split() for line in lines]
-
-    hands = sorted(lines, key=lambda v: order(v[0]))
-
-    return sum((i + 1) * int(hand[1]) for i, hand in enumerate(hands))
+    return solve(lines, order)
 
 
 def part2(lines):
-    lines = [line.split() for line in lines]
-
-    hands = sorted(lines, key=lambda v: order_alt(v[0]))
-
-    return sum((i + 1) * int(hand[1]) for i, hand in enumerate(hands))
+    return solve(lines, order_alt)
 
 
 TEST_DATA = {}
