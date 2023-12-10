9e87f574219c16631670bac8a223c782a562e47a
Atlante45 <clement.brisset@gmail.com>
2023-12-10 01:05:53 -0800

    2023 Day 10 refactor

--- solutions/y2023/d10.py
+++ solutions/y2023/d10.py
@@ -1,218 +1,74 @@
-from queue import PriorityQueue
-from typing import Counter
-
-from more_itertools import pairwise
 from solutions.utils.graph import dijkstra, neighbors4
+
+LEFT = "S7J-"
+RIGHT = "SFL-"
+UP = "SLJ|"
+DOWN = "SF7|"
+
+
+def find_start(grid, neighbors):
+    for i, row in enumerate(grid):
+        for j, c in enumerate(row):
+            if c == "S":
+                si, sj = i, j
+
+    ns = [(i - si, j - sj) for i, j in neighbors((si, sj))]
+    if (-1, 0) in ns and (1, 0) in ns:
+        grid[si][sj] = "|"
+    elif (-1, 0) in ns and (0, -1) in ns:
+        grid[si][sj] = "J"
+    elif (-1, 0) in ns and (0, 1) in ns:
+        grid[si][sj] = "L"
+    elif (1, 0) in ns and (0, -1) in ns:
+        grid[si][sj] = "7"
+    elif (1, 0) in ns and (0, 1) in ns:
+        grid[si][sj] = "F"
+    elif (0, -1) in ns and (0, 1) in ns:
+        grid[si][sj] = "-"
+
+    return (si, sj)
 
 
 def parse(data):
-    return data.splitlines()
+    grid = [list(line) for line in data.splitlines()]
 
-
-def find_start(lines, v="S"):
-    for i in range(len(lines)):
-        for j in range(len(lines[0])):
-            if lines[i][j] == v:
-                return i, j
-    return None
-
-
-def djik(start, neighbors):
-    starts = [start]
-    frontier = PriorityQueue()
-    came_from = {}
-    cost_so_far = {}
-
-    for start in starts:
-        frontier.put(start, 0)
-        came_from[start] = None
-        cost_so_far[start] = 0
-
-    while not frontier.empty():
-        current = frontier.get()
-
-        for next in neighbors(current):
-            new_cost = cost_so_far[current] + 1
-
-            if next in cost_so_far and cost_so_far[next] == new_cost:
-                return new_cost, [next, current], came_from
-
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                frontier.put(next, new_cost)
-                came_from[next] = current
-
-    return -1
-
-
-def part1(lines):
-    start = find_start(lines)
-
-    def next(p):
+    def neighbors(p):
         ns = []
         i, j = p
-        fro = lines[i][j]
-        for a, b in neighbors4(i, j, lines):
-            to = lines[a][b]
-            # print(fro, to, a, b)
-            if (
-                a == i - 1
-                and (fro == "L" or fro == "J" or fro == "|" or fro == "S")
-                and (to == "F" or to == "7" or to == "|")
-            ):
+        fro = grid[i][j]
+        for a, b in neighbors4(i, j, grid):
+            to = grid[a][b]
+            if a == i - 1 and fro in UP and to in DOWN:
                 ns += [(a, b)]
-            if (
-                a == i + 1
-                and (to == "L" or to == "J" or to == "|")
-                and (fro == "F" or fro == "7" or fro == "|" or fro == "S")
-            ):
+            elif a == i + 1 and fro in DOWN and to in UP:
                 ns += [(a, b)]
-            if (
-                b == j - 1
-                and (fro == "7" or fro == "J" or fro == "-" or fro == "S")
-                and (to == "F" or to == "L" or to == "-")
-            ):
+            elif b == j - 1 and fro in LEFT and to in RIGHT:
                 ns += [(a, b)]
-            if (
-                b == j + 1
-                and (to == "7" or to == "J" or to == "-")
-                and (fro == "F" or fro == "L" or fro == "-" or fro == "S")
-            ):
+            elif b == j + 1 and fro in RIGHT and to in LEFT:
                 ns += [(a, b)]
-        # print(p, ns)
         return ns
 
-    # print(start)
-    # print(next(start))
+    start = find_start(grid, neighbors)
 
-    res, _, _ = djik(start, next)
-    return res
-
-
-def propagate(lines, i, j, v):
-    ps = [(i, j)]
-
-    while len(ps) > 0:
-        i, j = ps.pop()
-        lines[i][j] = v
-        for a, b in neighbors4(i, j, lines):
-            if lines[a][b] == ".":
-                ps.append((a, b))
+    _, cost_so_far = dijkstra(start, neighbors)
+    return grid, cost_so_far
 
 
-def part2(lines):
-    start = find_start(lines)
+def part1(_, cost_so_far):
+    return len(cost_so_far.keys()) // 2
 
-    def next(p):
-        ns = []
-        i, j = p
-        fro = lines[i][j]
-        for a, b in neighbors4(i, j, lines):
-            to = lines[a][b]
-            # print(fro, to, a, b)
-            if (
-                a == i - 1
-                and (fro == "L" or fro == "J" or fro == "|" or fro == "S")
-                and (to == "F" or to == "7" or to == "|")
-            ):
-                ns += [(a, b)]
-            if (
-                a == i + 1
-                and (to == "L" or to == "J" or to == "|")
-                and (fro == "F" or fro == "7" or fro == "|" or fro == "S")
-            ):
-                ns += [(a, b)]
-            if (
-                b == j - 1
-                and (fro == "7" or fro == "J" or fro == "-" or fro == "S")
-                and (to == "F" or to == "L" or to == "-")
-            ):
-                ns += [(a, b)]
-            if (
-                b == j + 1
-                and (to == "7" or to == "J" or to == "-")
-                and (fro == "F" or fro == "L" or fro == "-" or fro == "S")
-            ):
-                ns += [(a, b)]
-        # print(p, ns)
-        return ns
 
-    # print(start)
-    # print(next(start))
-
-    _, fros, came_from = djik(start, next)
-
-    lines = [list(line) for line in lines]
-    # print("\n".join(["".join(l) for l in lines]))
-
-    path = fros
-    a, b = fros
-    while a is not None:
-        a = came_from[a]
-        path.append(a)
-    while b is not None:
-        b = came_from[b]
-        path.append(b)
-
-    for i in range(len(lines)):
-        for j in range(len(lines[0])):
-            if lines[i][j] in ["J", "L", "7", "F", "|", "-"] and (i, j) not in path:
-                lines[i][j] = "."
-
-    # start = find_start(lines, ".")
-    # areas = []
-    # while start is not None:
-    #     areas.append(propagate(lines, *start, "I"))
-    #     start = find_start(lines, ".")
-    # print(areas)
-
-    # print("\n".join(["".join(l) for l in lines]))
-
-    new_lines = []
-    for line in lines:
-        new_line = [line[0]]
-        for a, b in pairwise(line):
-            new_line += [
-                "-" if a in ["F", "L", "-", "S"] and b in ["7", "J", "-", "S"] else ".",
-                b,
-            ]
-        new_lines.append(new_line)
-
-    lines = new_lines
-
-    # print("\n")
-    # print("\n".join(["".join(l) for l in lines]))
-
-    new_lines = [lines[0]]
-    for a, b in pairwise(lines):
-        new_line = []
-        for i in range(len(lines[0])):
-            new_line.append(
-                "|"
-                if a[i] in ["F", "7", "|", "S"] and b[i] in ["L", "J", "|", "S"]
-                else "."
-            )
-        new_lines += [new_line, b]
-
-    lines = new_lines
-
-    # print("\n")
-    # print("\n".join(["".join(l) for l in lines]))
-
-    for i in range(len(lines)):
-        for j in range(len(lines[0])):
-            if (
-                i == 0 or j == 0 or i == len(lines) - 1 or j == len(lines[0]) - 1
-            ) and lines[i][j] == ".":
-                propagate(lines, i, j, "O")
-
-    # print("\n".join(["".join(l) for l in lines]))
+def part2(grid, cost_so_far):
+    loop = cost_so_far.keys()
 
     res = 0
-    for i in range(len(lines)):
-        for j in range(len(lines[0])):
-            if i % 2 == 0 and j % 2 == 0 and lines[i][j] == ".":
+    for i, row in enumerate(grid):
+        inside = False
+        for j, c in enumerate(row):
+            if inside and (i, j) not in loop:
                 res += 1
+            elif c in UP and (i, j) in loop:
+                inside = not inside
     return res
 
 
