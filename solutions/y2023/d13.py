d1e8c6c40d8ceff7994055f1cffaa0fc48f9ee30
Atlante45 <clement.brisset@gmail.com>
2023-12-15 01:09:41 -0800

    2023 Day 13 Refactor

--- solutions/y2023/d13.py
+++ solutions/y2023/d13.py
@@ -1,52 +1,32 @@
 def check_mirror(pattern, mirror):
-    return all(
-        pattern[mirror - i - 1] == pattern[mirror + i]
+    return sum(
+        int(pattern[mirror - i - 1][j] != pattern[mirror + i][j])
         for i in range(min(mirror, len(pattern) - mirror))
+        for j in range(len(pattern[0]))
     )
 
 
-def find_mirrors(pattern):
-    return [m for m in range(1, len(pattern)) if check_mirror(pattern, m)]
+def find_mirrors(pattern, offset):
+    return [m for m in range(1, len(pattern)) if check_mirror(pattern, m) == offset]
 
 
-def summarize(pattern):
+def summarize(pattern, offset):
     zipped = [list(a) for a in zip(*pattern)]
-    vert = find_mirrors(pattern)
-    horiz = find_mirrors(zipped)
-    return vert, horiz
-
-
-def score(pattern):
-    v, h = summarize(pattern)
-    return 100 * sum(v) + sum(h)
-
-
-def smudge(pattern):
-    v, h = summarize(pattern)
-    for i, line in enumerate(pattern):
-        for j, c in enumerate(line):
-            pattern[i][j] = "." if c == "#" else "#"
-            rv, rh = summarize(pattern)
-            rv = [r for r in rv if r not in v]
-            rh = [r for r in rh if r not in h]
-            if len(rv) > 0 or len(rh) > 0:
-                return 100 * sum(rv) + sum(rh)
-
-            pattern[i][j] = c
+    vert = find_mirrors(pattern, offset)
+    horiz = find_mirrors(zipped, offset)
+    return 100 * sum(vert) + sum(horiz)
 
 
 def parse(data):
-    return [
-        [list(line) for line in pattern.splitlines()] for pattern in data.split("\n\n")
-    ]
+    return [[list(row) for row in grid.splitlines()] for grid in data.split("\n\n")]
 
 
 def part1(patterns):
-    return sum(score(p) for p in patterns)
+    return sum(summarize(p, 0) for p in patterns)
 
 
 def part2(patterns):
-    return sum(smudge(pattern) for pattern in patterns)
+    return sum(summarize(p, 1) for p in patterns)
 
 
 TEST_DATA = {}
