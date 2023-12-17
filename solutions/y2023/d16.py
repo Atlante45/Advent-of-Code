4b049c630db59278025592f75e31553186757be3
Atlante45 <clement.brisset@gmail.com>
2023-12-17 05:53:59 +0100

    2023 Day 16 refactor

--- solutions/y2023/d16.py
+++ solutions/y2023/d16.py
@@ -1,109 +1,55 @@
 from collections import defaultdict, deque
 
 
-DIR = {
-    ">": (0, 1),
-    "<": (0, -1),
-    "v": (1, 0),
-    "^": (-1, 0),
+DIR = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+ITEM = {
+    ".": [[0], [1], [2], [3]],
+    "/": [[1], [0], [3], [2]],
+    "\\": [[3], [2], [1], [0]],
+    "|": [[0], [0, 2], [2], [0, 2]],
+    "-": [[3, 1], [1], [3, 1], [3]],
 }
+
+
+def valid(grid, i, j):
+    return i >= 0 and j >= 0 and i < len(grid) and j < len(grid[0])
 
 
 def parse(data):
     return [list(line) for line in data.splitlines()]
 
 
-def next(grid, i, j, d):
-    i += DIR[d][0]
-    j += DIR[d][1]
-
-    if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]):
-        return None, None
-
-    return i, j
-
-
 def solve(grid, i, j, d):
-    beams = deque()
-    beams.append(((i, j), d))
-
     history = defaultdict(list)
-    history[(i, j)] = [d]
 
-    def move(i, j, d):
-        i, j = next(grid, i, j, d)
-        if i is None or d in history[(i, j)]:
-            return
+    frontier = deque()
+    frontier.append((i, j, d))
+    while len(frontier) > 0:
+        i, j, d = frontier.pop()
+        if not valid(grid, i, j) or ((i, j) in history and d in history[(i, j)]):
+            continue
         history[(i, j)].append(d)
-        beams.appendleft(((i, j), d))
 
-    while len(beams) > 0:
-        (i, j), direction = beams.popleft()
-        # print(i, j, direction, grid[i][j], beams)
+        frontier.extend(
+            [(i + DIR[dir][0], j + DIR[dir][1], dir) for dir in ITEM[grid[i][j]][d]]
+        )
 
-        match grid[i][j]:
-            case ".":
-                move(i, j, direction)
-            case "/":
-                match direction:
-                    case ">":
-                        move(i, j, "^")
-                    case "<":
-                        move(i, j, "v")
-                    case "^":
-                        move(i, j, ">")
-                    case "v":
-                        move(i, j, "<")
-            case "\\":
-                match direction:
-                    case ">":
-                        move(i, j, "v")
-                    case "<":
-                        move(i, j, "^")
-                    case "^":
-                        move(i, j, "<")
-                    case "v":
-                        move(i, j, ">")
-            case "|":
-                match direction:
-                    case ">":
-                        move(i, j, "v")
-                        move(i, j, "^")
-                    case "<":
-                        move(i, j, "v")
-                        move(i, j, "^")
-                    case "^":
-                        move(i, j, "^")
-                    case "v":
-                        move(i, j, "v")
-            case "-":
-                match direction:
-                    case "^":
-                        move(i, j, "<")
-                        move(i, j, ">")
-                    case "v":
-                        move(i, j, "<")
-                        move(i, j, ">")
-                    case ">":
-                        move(i, j, ">")
-                    case "<":
-                        move(i, j, "<")
     return len(history)
 
 
 def part1(grid):
-    return solve(grid, 0, 0, ">")
+    return solve(grid, 0, 0, 1)
 
 
 def part2(grid):
     res = 0
     for i in range(len(grid)):
-        res = max(res, solve(grid, i, 0, ">"))
-        res = max(res, solve(grid, i, len(grid[0]) - 1, "<"))
+        res = max(res, solve(grid, i, 0, 1))
+        res = max(res, solve(grid, i, len(grid[0]) - 1, 3))
 
     for i in range(len(grid[0])):
-        res = max(res, solve(grid, 0, i, "v"))
-        res = max(res, solve(grid, len(grid) - 1, i, "^"))
+        res = max(res, solve(grid, 0, i, 2))
+        res = max(res, solve(grid, len(grid) - 1, i, 0))
     return res
 
 
