a1c16583096032a4a8186e22dd5db30dfa02ae38
Atlante45 <clement.brisset@gmail.com>
2023-12-19 09:02:14 +0100

    2023 Day 19 Refactor

--- solutions/y2023/d19.py
+++ solutions/y2023/d19.py
@@ -1,104 +1,71 @@
+import copy
 from math import prod
-import re
-
-
-REGEX = r"{x=(.*),m=(.*),a=(.*),s=(.*)}"
-
-
-def parse_workflow(workflow):
-    name, rules = workflow.split("{")
-    rules = rules[:-1].split(",")
-    return name, rules
 
 
 def parse(data):
     wfs, parts = data.split("\n\n")
     workflows = {}
     for wf in wfs.splitlines():
-        name, rules = parse_workflow(wf)
+        name, rules = wf.split("{")
+        rules = [r.split(":") if ":" in r else [None, r] for r in rules[:-1].split(",")]
         workflows[name] = rules
     parts = [
-        list(map(int, re.match(REGEX, part).groups())) for part in parts.splitlines()
+        eval(part.replace("=", ":"), {v: v for v in "xmas"})
+        for part in parts.splitlines()
     ]
     return workflows, parts
 
 
-INDEX = "xmas"
-
-
-def matcth_rule(rule, part):
-    if rule[1] == "<":
-        return part[INDEX.index(rule[0])] < int(rule[2:])
-    else:
-        return part[INDEX.index(rule[0])] > int(rule[2:])
-
-
-def is_acceptable(workflows, workflow, part):
+def accepted(workflows, workflow, part):
     if workflow == "A":
         return True
     if workflow == "R":
         return False
+
     workflow = workflows[workflow]
-    for rule in workflow:
-        if ":" in rule:
-            if matcth_rule(rule.split(":")[0], part):
-                return is_acceptable(workflows, rule.split(":")[1], part)
-        else:
-            return is_acceptable(workflows, rule, part)
+    for rule, dest in workflow:
+        if rule is None or eval(rule, part.copy()):
+            return accepted(workflows, dest, part)
 
 
 def part1(workflows, parts):
-    res = 0
-    for part in parts:
-        if is_acceptable(workflows, "in", part):
-            res += sum(part)
-    return res
+    return sum(sum(p.values()) for p in parts if accepted(workflows, "in", p))
 
 
 def split_part(rule, part):
-    ind = INDEX.index(rule[0])
-    op = rule[1]
+    if rule is None:
+        return part
+
+    ind = rule[0]
     val = int(rule[2:])
 
-    part1 = [p.copy() for p in part]
-    part2 = [p.copy() for p in part]
-    if op == "<":
-        part1[ind][1] = min(part1[ind][1], val - 1)
-        part2[ind][0] = max(part1[ind][0], val)
+    accepted_part = copy.deepcopy(part)
+    rejected_part = part
+    if rule[1] == "<":
+        accepted_part[ind][1] = min(accepted_part[ind][1], val - 1)
+        rejected_part[ind][0] = max(rejected_part[ind][0], val)
     else:
-        part1[ind][0] = max(part1[ind][0], val + 1)
-        part2[ind][1] = min(part1[ind][1], val)
+        accepted_part[ind][0] = max(accepted_part[ind][0], val + 1)
+        rejected_part[ind][1] = min(rejected_part[ind][1], val)
 
-    return part1 if part1[ind][0] <= part1[ind][1] else None, part2 if part2[ind][
-        0
-    ] <= part2[ind][1] else None
+    return accepted_part if accepted_part[ind][0] <= accepted_part[ind][1] else None
 
 
 def compute(workflows, part, workflow):
     if workflow == "R" or part is None:
         return 0
     if workflow == "A":
-        return prod([p[1] - p[0] + 1 for p in part])
+        return prod([p[1] - p[0] + 1 for p in part.values()])
 
-    workflow = workflows[workflow]
     res = 0
-    for rule in workflow:
-        if ":" in rule:
-            part1, part2 = split_part(rule.split(":")[0], part)
-            res += compute(workflows, part1, rule.split(":")[1])
-            part = part2
-        else:
-            res += compute(workflows, part, rule)
+    for rule, dest in workflows[workflow]:
+        accepted_part = split_part(rule, part)
+        res += compute(workflows, accepted_part, dest)
     return res
 
 
-def part2(workflows, parts):
-    part = [
-        [1, 4000],
-        [1, 4000],
-        [1, 4000],
-        [1, 4000],
-    ]
+def part2(workflows, _):
+    part = {v: [1, 4000] for v in "xmas"}
     return compute(workflows, part, "in")
 
 
