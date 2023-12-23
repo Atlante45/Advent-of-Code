d9f2a356ae5ad83d2e8d6491c6cb68f65fc9ba75
Atlante45 <clement.brisset@gmail.com>
2023-12-23 08:46:22 +0100

    2023 Day 23 Refactor

--- solutions/y2023/d23.py
+++ solutions/y2023/d23.py
@@ -1,6 +1,4 @@
 from collections import defaultdict
-import dis
-import heapq
 from solutions.utils.graph import neighbors4
 
 
@@ -16,102 +14,68 @@ DIR = {
 }
 
 
-def part1(lines):
-    return 94 if len(lines) < 30 else 2394
-    res = 0
-
-    heap = []
-    heapq.heappush(heap, (1, [(0, 1)]))
-    while heap:
-        _, path = heapq.heappop(heap)
-        x, y = path[-1]
-        ns = []
-        if lines[x][y] == ".":
-            ns = [(x, y) for x, y in neighbors4(x, y, lines) if lines[x][y] != "#"]
-        else:
-            ns = [(x + DIR[lines[x][y]][0], y + DIR[lines[x][y]][1])]
-        for n in ns:
-            if n not in path:
-                heapq.heappush(heap, (len(path) + 1, path + [n]))
-            elif (x, y) == (len(lines) - 1, len(lines[0]) - 2):
-                res = max(res, len(path))
-
-    return res - 1
-
-
-def follow_path(lines, i, j):
-    c = lines[i][j]
-    assert c in DIR
-
-    path = [(i, j), (i + DIR[c][0], j + DIR[c][1])]
-    while True:
-        x, y = path[-1]
-        if lines[x][y] in DIR:
-            break
-
-        ns = [(a, b) for a, b in neighbors4(x, y, lines) if lines[a][b] != "#"]
-        ns.remove(path[-2])
-        if len(ns) == 0:
-            break
-        # print(x, y, ns)
-        assert len(ns) == 1
-        path.append(ns[0])
-
-    i, j = path[-1]
-    return len(path), next(lines, i, j)
-
-
 def next(lines, i, j):
-    assert lines[i][j] in DIR
     return (i + DIR[lines[i][j]][0], j + DIR[lines[i][j]][1])
 
 
-def part2(lines):
-    # if len(lines) < 30:
-    #     return 154
+def follow_path(lines, i, j):
+    path = [(i, j), next(lines, i, j)]
+    while lines[path[-1][0]][path[-1][1]] not in DIR:
+        ns = [(a, b) for a, b in neighbors4(*path[-1], lines) if lines[a][b] != "#"]
+        ns.remove(path[-2])
+        path.append(ns[0])
+    return len(path) + 1, next(lines, *path[-1])
 
-    start = (-1, 1)
-    goal = (len(lines), len(lines[0]) - 2)
 
+def compute_graph(lines, start):
     lines[0][1] = "v"
     lines[-1][-2] = "v"
 
     nodes = defaultdict(list)
     edges = dict()
-    starts = [start]
 
+    starts = [start]
     while starts:
         i, j = starts.pop()
         for x, y in neighbors4(i, j, lines):
             if lines[x][y] in DIR and next(lines, x, y) != (i, j):
                 dist, dest = follow_path(lines, x, y)
+
                 if dest not in nodes:
                     starts.append(dest)
 
                 nodes[(i, j)].append(dest)
+                edges[(i, j), dest] = dist
+
                 nodes[dest].append((i, j))
-                edges[(i, j), dest] = dist + 1
-                edges[dest, (i, j)] = dist + 1
-                print((i, j), dest, dist)
-    # print(nodes[goal])
+                edges[dest, (i, j)] = -dist
 
-    res = 0
+    return nodes, edges
 
-    heap = []
-    heapq.heappush(heap, (0, [start]))
-    while heap:
-        dist, path = heapq.heappop(heap)
-        if path[-1] == goal:
-            if dist > res:
-                print(dist, path)
-                res = dist
-            continue
-        for n in nodes[path[-1]]:
-            if n in path:
-                continue
-            heapq.heappush(heap, (dist + edges[path[-1], n], path + [n]))
 
-    return res - 2
+def parts(lines):
+    start = (-1, 1)
+    goal = (len(lines), len(lines[0]) - 2)
+
+    nodes, edges = compute_graph(lines, start)
+
+    res = [0, 0]
+
+    def dfs(current, cost, visited, directed):
+        if current == goal:
+            res[0] = cost if cost > res[0] and directed else res[0]
+            res[1] = cost if cost > res[1] else res[1]
+            return
+
+        for n in nodes[current]:
+            if n not in visited:
+                visited.add(n)
+                weight = edges[current, n]
+                dfs(n, cost + abs(weight), visited, directed and weight > 0)
+                visited.remove(n)
+
+    dfs(start, -2, set([start]), True)
+    return res
 
 
 TEST_DATA = {}
