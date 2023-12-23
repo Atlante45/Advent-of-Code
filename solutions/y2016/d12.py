b4fd5ecc77982ead41542bb30ead30cd4876ede4
Atlante45 <clement.brisset@gmail.com>
2023-12-23 23:24:44 +0100

    Cleanup

--- solutions/y2016/d12.py
+++ solutions/y2016/d12.py
@@ -2,9 +2,7 @@ def parse(data):
     return [line.split() for line in data.splitlines()]
 
 
-def part1(instructions):
-    registers = {"a": 0, "b": 0, "c": 0, "d": 0}
-
+def asmbunny(instructions, registers):
     ipr = 0
     while ipr >= 0 and ipr < len(instructions):
         inst = instructions[ipr]
@@ -25,32 +23,18 @@ def part1(instructions):
                     ipr += int(inst[2])
                     continue
         ipr += 1
+    return registers
+
+
+def part1(instructions):
+    registers = {"a": 0, "b": 0, "c": 0, "d": 0}
+    asmbunny(instructions, registers)
     return registers["a"]
 
 
 def part2(instructions):
     registers = {"a": 0, "b": 0, "c": 1, "d": 0}
-
-    ipr = 0
-    while ipr >= 0 and ipr < len(instructions):
-        inst = instructions[ipr]
-
-        match inst[0]:
-            case "cpy":
-                if inst[1] in registers:
-                    registers[inst[2]] = registers[inst[1]]
-                else:
-                    registers[inst[2]] = int(inst[1])
-            case "inc":
-                registers[inst[1]] += 1
-            case "dec":
-                registers[inst[1]] -= 1
-            case "jnz":
-                v = registers[inst[1]] if inst[1] in registers else int(inst[1])
-                if v != 0:
-                    ipr += int(inst[2])
-                    continue
-        ipr += 1
+    asmbunny(instructions, registers)
     return registers["a"]
 
 
