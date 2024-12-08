4c5a2b7222cac9ffcbbcc2dde8b5b9c8e6a85224
Atlante45 <clement.brisset@gmail.com>
2024-12-07 20:58:31 -0800

    2024 Day 7 Refactor

--- solutions/y2024/d07.py
+++ solutions/y2024/d07.py
@@ -1,17 +1,12 @@
 def parse(data):
-    return data.splitlines()
-
-def recurse(num, vals):
-    if len(vals) == 1:
-        return num == vals[0]
-
-    res = recurse(num - vals[-1], vals[:-1])
-    if num % vals[-1] == 0:
-        res |= recurse(num // vals[-1], vals[:-1])
-
+    res = []
+    for line in data.splitlines():
+        line = line.split(":")
+        res.append((int(line[0]), [int(x) for x in line[1].split()]))
     return res
 
-def recurse2(num, vals):
+
+def recurse(num, vals, do_concat=False):
     if len(vals) == 1:
         return num == vals[0]
     
@@ -19,61 +14,34 @@ def recurse2(num, vals):
 
     res = False
     if num > val:
-        res = recurse2(num - val, vals[:-1])
+        res |= recurse(num - val, vals[:-1], do_concat)
     if num % val == 0:
-        res |= recurse2(num // val, vals[:-1])
-
-    if str(num).endswith(str(val)) and len(str(num)) > len(str(val)):
-        res |= recurse2(int(str(num)[:-len(str(val))]), vals[:-1])
-    
-
-    return res
-
-def part1(lines):
-    res = 0
-
-    for line in lines:
-        line = line.split(":")
-        num = int(line[0])
-        vals = [int(x) for x in line[1].split()]
-
-
-        if recurse(num, vals):
-            res += num
+        res |= recurse(num // val, vals[:-1], do_concat)
+    if do_concat and str(num).endswith(str(val)) and len(str(num)) > len(str(val)):
+        res |= recurse(int(str(num)[:-len(str(val))]), vals[:-1], do_concat)
 
     return res
 
 
-def part2(lines):
-    res = 0
+def part1(eqs):
+    return sum(eq[0] for eq in eqs if recurse(eq[0], eq[1]))
 
-    for line in lines:
-        line = line.split(":")
-        num = int(line[0])
-        vals = [int(x) for x in line[1].split()]
 
-        if recurse2(num, vals):
-            res += num
-
-    return res
+def part2(eqs):
+    return sum(eq[0] for eq in eqs if recurse(eq[0], eq[1], do_concat=True))
 
 
 TEST_DATA = {}
 TEST_DATA[
     """\
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
 192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
 """.rstrip()
-] = (None, 192)
-# TEST_DATA[
-#     """\
-# 190: 10 19
-# 3267: 81 40 27
-# 83: 17 5
-# 156: 15 6
-# 7290: 6 8 6 15
-# 161011: 16 10 13
-# 192: 17 8 14
-# 21037: 9 7 18 13
-# 292: 11 6 16 20
-# """.rstrip()
-# ] = (3749, 11387)
+] = (3749, 11387)
