535987ae30b7665f9ad269cb2dae4e4c092e9023
Atlante45 <clement.brisset@gmail.com>
2024-12-21 10:31:43 +0100

    2024 Day 21 Refactor

--- solutions/y2024/d21.py
+++ solutions/y2024/d21.py
@@ -1,45 +1,8 @@
 from functools import cache
 from itertools import pairwise
-import re
-from solutions.utils.graph import neighbors4
-
-
 from collections import defaultdict
-from queue import PriorityQueue
 
-
-def dijkstra(starts, neighbors, cost=None):
-    if not isinstance(starts, list):
-        starts = [starts]
-
-    frontier = PriorityQueue()
-    came_from = defaultdict(list)
-    cost_so_far = {}
-
-    for start in starts:
-        frontier.put(start, 0)
-        cost_so_far[start] = 0
-
-    while not frontier.empty():
-        current = frontier.get()
-
-        for next in neighbors(current):
-            new_cost = cost_so_far[current] + 1
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                frontier.put(next, new_cost)
-                came_from[next] = [current]
-            if (
-                next in cost_so_far
-                and new_cost == cost_so_far[next]
-                and current not in came_from[next]
-            ):
-                came_from[next].append(current)
-    return came_from, cost_so_far
-
-
-def parse(data):
-    return data.splitlines()
+from solutions.utils.graph import dijkstra_full, neighbors4
 
 
 K1 = (("7", "8", "9"), ("4", "5", "6"), ("1", "2", "3"), (None, "0", "A"))
@@ -47,41 +10,55 @@ K2 = ((None, "^", "A"), ("<", "v", ">"))
 DIRS = {(0, -1): "<", (1, 0): "v", (0, 1): ">", (-1, 0): "^"}
 
 
-@cache
-def find_move_sequences(K, v1, v2):
-    if v1 == v2:
-        return [("A",)]
-
-    sx, sy = None, None
-    ex, ey = None, None
+def map_grid(K):
+    neighbors = defaultdict(list)
+    directions = {}
     for i, row in enumerate(K):
         for j, v in enumerate(row):
-            if v == v1:
-                sx, sy = i, j
-            if v == v2:
-                ex, ey = i, j
+            if v is None:
+                continue
 
-    def ns(n):
-        return [
-            (x, y) for x, y in neighbors4(*n, len(K), len(K[0])) if K[x][y] is not None
-        ]
+            for x, y in neighbors4(i, j, len(K), len(K[0])):
+                if K[x][y] is not None:
+                    neighbors[v].append(K[x][y])
+                    directions[v, K[x][y]] = DIRS[x - i, y - j]
 
-    came_from, _ = dijkstra((sx, sy), ns)
+    return neighbors, directions
 
-    all_paths = [[(ex, ey)]]
-    while not all(path[0] == (sx, sy) for path in all_paths):
+
+NEIGHBORS1, DIRECTIONS1 = map_grid(K1)
+NEIGHBORS2, DIRECTIONS2 = map_grid(K2)
+
+
+def parse(data):
+    return data.splitlines()
+
+
+@cache
+def find_move_sequences(K, from_key, pressed_key):
+    if from_key == pressed_key:
+        return [("A",)]
+
+    if K == K1:
+        NEIGHBORS = NEIGHBORS1
+        DIRECTIONS = DIRECTIONS1
+    else:
+        NEIGHBORS = NEIGHBORS2
+        DIRECTIONS = DIRECTIONS2
+
+    came_from, _ = dijkstra_full(from_key, lambda n: NEIGHBORS[n])
+
+    all_paths = [[pressed_key]]
+    while not all(path[0] == from_key for path in all_paths):
         new_paths = []
         for path in all_paths:
-            head = path[0]
-            for f in came_from[head]:
-                new_paths.append([f] + path)
+            new_paths.extend([[f] + path for f in came_from[path[0]]])
         all_paths = new_paths
 
-    res = []
-    for path in all_paths:
-        rp = [DIRS[x2 - x1, y2 - y1] for (x1, y1), (x2, y2) in pairwise(path)]
-        res.append(tuple(rp + ["A"]))
-    return res
+    return [
+        tuple([DIRECTIONS[a, b] for a, b in pairwise(path)] + ["A"])
+        for path in all_paths
+    ]
 
 
 @cache
@@ -101,7 +78,7 @@ def shortest_sequence_length(K, code, depth):
 
 
 def complexity(code, num_robots):
-    numeric_code = int("".join(re.findall(r"\d", code)))
+    numeric_code = int(code[:-1])
     sequence_length = shortest_sequence_length(K1, code, num_robots)
     return numeric_code * sequence_length
 
